0e48fbbf04084070eb9f11637ab2f03dcdc294b1
Franklin-PC <Franklin-PC@DESKTOP-HI3UNOV>
2020-10-05 21:21:22 -0700

    second version

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,30 +1,44 @@
 package cse360assignment02;
 
 /**
- * @author Franklin-PC
+ * @author Franklin Paul
  *
  */
+
 public class AddingMachine {
   private int total;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
+  //returns total
   public int getTotal () {
-    return 0;
+    return total;
   }
   
+  // adds the value to the total and concatenates " + number" to the history
   public void add (int value) {
+	  total = total + value;
+	  history = history + " + " + value;
   }
 
+  // subtracts the value from the total and concatenates " - number" to the history
   public void subtract (int value) {
+	  total = total - value;
+	  history = history + " - " + value;
   }
 
+  // returns the history 
   public String toString () {
-    return "";
+    return history;
   }
 
+  // initializes the total and history variables to their default
   public void clear() {
+	  total = 0;
+	  history = "0";
   }
 }
